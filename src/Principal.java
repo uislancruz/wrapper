5278da58dda498cd541d538f228e9b0067a21b42
Uislan Cruz <uislansk8@hotmail.com>
2025-05-26 20:43:47 -0300

    Trocando o tipo primitivo

---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        int idade = 20;
+        short idadeShort = (short) idade;
+
+        Integer diasEntrega = Integer.valueOf(30);
+        short diasEnntrefaShort = diasEntrega.shortValue();
+
+        Double valorToral = Double.valueOf(1500.2);
+    }
+}
